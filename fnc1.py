86e494fe2fa179df2be5a28e7e025bc2f6ce8c95
Seshagiri Sakthimani <124811651+seshagiri01@users.noreply.github.com>
2024-11-29 09:59:15 +0000

    Update fnc1.py

--- fnc1.py
+++ fnc1.py
@@ -7,13 +7,14 @@ import pwmio
 import math
 
 class serial_dilution():
-    def __init__(self, sensor, led) -> None:
+    def __init__(self, sensor, led):
         self.n_samples = int(input("Enter the Number of Samples: "))
         self.n_replicates = int(input("Enter the Number of Replicates: "))
         self.sensor = sensor
         self.led = led
+        pass
 
-    def setintegration(self, intTime) -> None:
+    def setintegration(self, intTime):
         stepSize = 9999
         self.sensor.astep = stepSize
         self.sensor.atime = int(intTime*1000/(2.78*(stepSize+1)))
@@ -31,12 +32,12 @@ class serial_dilution():
         print("630nm " + str(sensor_channels[6]))
         print("680nm " + str(sensor_channels[7]))
         sesor_reading = dict(zip(wavelengths, sensor_channels))
-        trans = (brightness/sesor_reading[wavelength])
-        absorb = math.log(1/trans, base=10)
-        print(absorb)
-        return absorb
+        trans = (sesor_reading[wavelength]/brightness)
+        absorbance = -1* math.log(trans,10)
+        print(absorbance)
+        return sesor_reading[wavelength]
 
-    
+
     def final_reading(self, wavelength: int, brightness_percentage: int = 100, integration_time : int = 100, sensor_gain : int = 7) -> dict:
         result_dict = {}
         print("Calibrating LED brightness")
@@ -52,13 +53,13 @@ class serial_dilution():
         self.setintegration(integration_time)
         print("\n")
         print("Getting ready for samples...")
-        result_dict['ctrl'] = self.read(wavelength, brightness)
         for i in range(self.n_replicates):
             result_dict[f'replicate_{i}'] = {}
             print(f"Replicate {i+1}")
             print("Time to read the control!")
             input("Press Enter once you have loaded the control: ")
-            for j in range(self.n_samples-1):
+            ctrl_brightness = self.read(wavelength, brightness)
+            for j in range(self.n_samples):
                 input(f"Press Enter once you have loaded the sample {j+1}: ")
-                result_dict[f'replicate_{i}'][f'sample_{j+1}'] = self.read(wavelength, brightness)
+                result_dict[f'replicate_{i}'][f'sample_{j+1}'] = self.read(wavelength, ctrl_brightness)
         return(result_dict)
